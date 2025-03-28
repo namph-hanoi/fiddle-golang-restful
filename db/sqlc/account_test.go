5ba7b23b59d7fbfb34723fa7838675e2e9c091d0
Nam Phan <namph.tech@gmail.com>
2025-03-28 09:36:47 +0700

    practice: add and modify tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,12 +3,13 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/namph-hanoi/fiddle-golang-restful/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) (Account, CreateAccountParams) {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -17,6 +18,11 @@ func TestCreateAccount(t *testing.T) {
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
+	return account, arg
+}
+
+func TestCreateAccount(t *testing.T) {
+	account, arg := createRandomAccount(t)
 	require.NotEmpty(t, account)
 
 	require.Equal(t, arg.Owner, account.Owner)
@@ -25,4 +31,14 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+}
+func TestGetAccount(t *testing.T) {
+	account, arg := createRandomAccount(t)
+	response, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	require.Equal(t, response.Balance, arg.Balance)
+	require.Equal(t, response.Currency, arg.Currency)
+	require.Equal(t, response.Owner, arg.Owner)
+	require.WithinDuration(t, account.CreatedAt, response.CreatedAt, 3*time.Second)
 }
