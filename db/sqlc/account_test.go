7fa6a0bea718f5fade3bd00c3bc60bef098286d8
Nam Phan <namph.tech@gmail.com>
2025-05-16 22:10:23 +0700

    feat: add another test for transfer

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -83,3 +83,23 @@ func TestListAccount(t *testing.T) {
 	}
 
 }
+
+func TestCreatingTransfer(t *testing.T) {
+	account1, _ := createRandomAccount(t)
+	account2, _ := createRandomAccount(t)
+	amount := int64(10)
+	transfer, err := testQueries.CreateTransfer(context.Background(), CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        amount,
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+	require.Equal(t, transfer.FromAccountID, account1.ID)
+	require.Equal(t, transfer.ToAccountID, account2.ID)
+	require.Equal(t, transfer.Amount, amount)
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+	_, err = testQueries.GetTransfer(context.Background(), transfer.ID)
+	require.NoError(t, err)
+}
