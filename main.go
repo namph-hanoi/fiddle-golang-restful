bc59c016dc6b30e47a2e98bcb18b61ba77763df7
Nam Phan <namph.tech@gmail.com>
2025-05-17 23:13:54 +0700

    feat: load env using viper

--- main.go
+++ main.go
@@ -7,16 +7,21 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/namph-hanoi/fiddle-golang-restful/api"
 	db "github.com/namph-hanoi/fiddle-golang-restful/db/sqlc"
+	"github.com/namph-hanoi/fiddle-golang-restful/util"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load the config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,10 +29,10 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
-	log.Println("server started at", serverAddress)
+	log.Println("server started at", config.ServerAddress)
 
 }
