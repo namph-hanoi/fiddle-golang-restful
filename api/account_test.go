97a2362685700d86b332a7fde1e99564103b14e9
Nam Phan <namph.tech@gmail.com>
2025-05-18 15:20:46 +0700

    feat: first API test case

---
+++ api/account_test.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/golang/mock/gomock"
+	mockdb "github.com/namph-hanoi/fiddle-golang-restful/db/mock"
+	db "github.com/namph-hanoi/fiddle-golang-restful/db/sqlc"
+	"github.com/namph-hanoi/fiddle-golang-restful/util"
+)
+
+func TestGetAccountApi(t *testing.T) {
+	account := randomAccount()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+
+	store.EXPECT().
+		GetAccount(gomock.Any(), gomock.Eq(account.ID)).
+		Times(1).
+		Return(account, nil)
+
+	server := NewServer(store)
+	recorder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/accounts/%d", account.ID)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	require.NoError(t, err)
+
+	server.router.ServeHTTP(recorder, request)
+
+	require.Equal(t, http.StatusOK, recorder.Code)
+}
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+	}
+}
