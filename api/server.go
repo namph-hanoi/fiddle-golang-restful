4c7d62f48bcfe19ea8fc09cc1f5e460cebee0b8a
Nam Phan <namph.tech@gmail.com>
2025-05-18 22:28:09 +0700

    feat: API for creating transfers

--- api/server.go
+++ api/server.go
@@ -19,6 +19,7 @@ func NewServer(store db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.GetAccount)
 	router.GET("/accounts", server.ListAccount)
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	for _, route := range router.Routes() {
