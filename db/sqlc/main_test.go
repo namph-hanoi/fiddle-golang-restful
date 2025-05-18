cbb23ea1559eb605ad42d275b0d99e15a9ec824b
Nam Phan <namph.tech@gmail.com>
2025-05-18 11:00:34 +0700

    feat: apply loading config in tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,22 +7,21 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/namph-hanoi/fiddle-golang-restful/util"
 )
 
 var testQueries *Queries
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
 
 var queries Queries
 
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannnot connect to the DB. Details: ", err)
 	}
