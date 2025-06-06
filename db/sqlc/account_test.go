4e765573396434923a7e2e84b654f98b46e3e6cb
Nam Phan <namph.tech@gmail.com>
2025-06-06 13:06:03 +0700

    fix: foreign key contraint error in account tests

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -10,8 +10,9 @@ import (
 )
 
 func createRandomAccount(t *testing.T) (Account, CreateAccountParams) {
+	user, _ := createRandomUser(t)
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
